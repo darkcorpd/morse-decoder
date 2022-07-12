70d6f9dd4906b385994bd05a1b1e436ce6e423f6
darkcorp <darkcorpd@gmail.com>
2022-07-12 22:45:35 +0300

    init: morse-decoder task start

--- src/index.js
+++ src/index.js
@@ -35,12 +35,44 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
-};
-
-function decode(expr) {
+    '**********': ' ',
+  };
+  
+  function decode(expr) {
     // write your solution here
-}
-
-module.exports = {
+    const binaries = chunk(expr,10); 
+    let result = '';
+    for (let i = 0; i < binaries.length; i++) {
+      const binary = chunk(binaries[i], 2);
+      const morseCode = toMorseCode(binary);
+      result += MORSE_TABLE[morseCode];
+    }
+  
+    return result;
+  }
+  
+  // Splits string into n-digit chunks
+  function chunk(str, size) {
+    return str.match(new RegExp('.{1,' + size + '}', 'g'));
+  }
+  
+  // Converts binary numbers to Morse code
+  function toMorseCode(arr) {
+    let code = '';
+    arr.forEach(cur => {
+        if (cur === '00') {
+          code += '';
+        } else if (cur === '10') {
+          code += '.';
+        } else if (cur === '11') {
+          code += '-';
+        } else if (cur === '**') {
+          code += '**';
+        }});
+      return code;
+  }
+  
+  
+  module.exports = {
     decode
-}
+  }
